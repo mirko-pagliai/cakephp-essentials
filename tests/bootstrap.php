f828e15609e469bb0f5960ef83ca9dcc316fc9ef
mirko-pagliai <mirko.pagliai@gmail.com>
2025-03-23 14:17:21 +0100

    added config for phpunit

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,2 +1,35 @@
 <?php
 declare(strict_types=1);
+
+use Cake\Core\Configure;
+
+date_default_timezone_set('UTC');
+mb_internal_encoding('UTF-8');
+ini_set('intl.default_locale', 'en_US');
+
+define('ROOT', dirname(__DIR__) . DS);
+const CORE_PATH = ROOT . 'vendor' . DS . 'cakephp' . DS . 'cakephp' . DS;
+const APP = ROOT . 'tests' . DS . 'test_app' . DS;
+const APP_DIR = 'test_app' . DS;
+const WWW_ROOT = APP . 'webroot' . DS;
+
+require dirname(__DIR__) . '/vendor/autoload.php';
+
+putenv('APP_DEFAULT_LOCALE=en_US');
+
+require_once CORE_PATH . 'config' . DS . 'bootstrap.php';
+
+Configure::write('debug', true);
+Configure::write('App', [
+    'namespace' => 'App',
+    'encoding' => 'UTF-8',
+    'base' => false,
+    'baseUrl' => false,
+    'dir' => APP_DIR,
+    'webroot' => 'webroot',
+    'wwwRoot' => WWW_ROOT,
+    'fullBaseUrl' => 'http://localhost',
+    'imageBaseUrl' => 'img/',
+    'jsBaseUrl' => 'js/',
+    'cssBaseUrl' => 'css/',
+]);
