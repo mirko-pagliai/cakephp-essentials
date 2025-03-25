b5bb3f462f8a2782b82ed35cf33fa32c247d84c3
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-03-25 12:32:53 +0100

    added paginator element

---
+++ templates/element/paginator.php
@@ -0,0 +1,53 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * @var \App\View\AppView $this
+ *
+ * @see https://github.com/FriendsOfCake/bootstrap-ui/blob/master/README.md#pagination
+ * @see https://getbootstrap.com/docs/5.3/components/pagination/
+ */
+
+use Cake\Core\Exception\CakeException;
+use function Cake\I18n\__d as __d;
+
+try {
+    $hasPages = $this->Paginator->hasPage();
+} catch (CakeException) {
+    $hasPages = false;
+}
+
+if (!$hasPages) {
+    return;
+}
+?>
+
+<nav class="d-print-none mt-4">
+    <div class="d-none d-lg-flex justify-content-center">
+        <?= $this->Paginator->links(options: ['prev' => true, 'next' => true]) ?>
+    </div>
+    <ul class="pagination d-lg-none justify-content-center m-0">
+        <?php
+        if ($this->Paginator->hasPrev()) {
+            echo $this->Paginator->prev();
+        }
+        echo $this->Html->tag(
+            name: 'li',
+            text: $this->Html->span(
+                text: $this->Paginator->counter(__d('cakephp/essentials', 'Page {{page}} of {{pages}}')),
+                options: ['class' => 'bg-transparent page-link']
+            ),
+            options: ['class' => 'page-item disabled']
+        );
+        if ($this->Paginator->hasNext()) {
+            echo $this->Paginator->next();
+        }
+        ?>
+    </ul>
+</nav>
+
+<div class="mt-3 small text-center text-secondary">
+    <?= $this->Paginator->counter(
+        __d('cakephp/essentials', 'Page {{page}} of {{pages}}, showing {{current}} items out of {{count}} total')
+    ) ?>
+</div>
---
+++ tests/test_app/TestApp/View/AppView.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+namespace App\View;
+
+use Cake\Essentials\View\View as EssentialsView;
+
+/**
+ * AppView.
+ */
+class AppView extends EssentialsView
+{
+}
