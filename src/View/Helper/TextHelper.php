17eb34c07c22c1920b3ec56c13cdc69238231111
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-03-25 08:24:15 +0100

    added `TextHelper`, with tests

---
+++ src/View/Helper/TextHelper.php
@@ -0,0 +1,71 @@
+<?php
+declare(strict_types=1);
+
+namespace Cake\Essentials\View\Helper;
+
+use Cake\View\Helper\TextHelper as CakeTextHelper;
+use Override;
+
+/**
+ * @inheritDoc
+ */
+class TextHelper extends CakeTextHelper
+{
+    /**
+     * Extends and improves the "autoParagraph" function.
+     *
+     * For example, this:
+     * ```
+     * This is a text
+     * This is another text
+     * ```
+     *
+     * becomes this:
+     * ```
+     * <p class="mb-2">This is a text</p>
+     * <p class="mb-0">This is another text</p>
+     * ```
+     *
+     * It does not apply margins to the last line (even when it is a single line).
+     * It automatically `trim()`s the lines.
+     *
+     * @param string|null $text
+     * @return string
+     */
+    #[Override]
+    public function autoParagraph(?string $text): string
+    {
+        if (!$text) {
+            return '';
+        }
+        if (!str_contains($text, PHP_EOL)) {
+            return '<p class="mb-0">' . trim($text) . '</p>';
+        }
+
+        $explodedText = array_filter(explode(PHP_EOL, $text));
+        $lastElement = end($explodedText);
+
+        foreach ($explodedText as $k => $sText) {
+            $explodedText[$k] = ($sText == $lastElement ? '<p class="mb-0">' : '<p class="mb-2">') . trim($sText) . '</p>';
+        }
+
+        return implode(PHP_EOL, $explodedText);
+    }
+
+    /**
+     * Highlights a given phrase in a text.
+     *
+     * @param string $text Text to search the phrase in
+     * @param string|string[] $phrase The phrase or phrases that will be searched
+     * @param array<string, mixed> $options An array of HTML attributes and options
+     * @return string The highlighted text
+     *
+     * @see \Cake\Utility\Text::highlight() for all options
+     */
+    public function highlight(string $text, array|string $phrase, array $options = []): string
+    {
+        $options += ['format' => '<span class="text-bg-warning">\1</span>'];
+
+        return parent::highlight(text: $text, phrase: $phrase, options: $options);
+    }
+}
---
+++ tests/TestCase/View/Helper/TextHelperTest.php
@@ -0,0 +1,100 @@
+<?php
+declare(strict_types=1);
+
+namespace Cake\Essentials\Test\TestCase\View\Helper;
+
+use Cake\Essentials\View\Helper\TextHelper;
+use Cake\TestSuite\TestCase;
+use Cake\View\View;
+use Generator;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
+
+/**
+ * TextHelperTest.
+ */
+#[CoversClass(TextHelper::class)]
+class TextHelperTest extends TestCase
+{
+    /**
+     * @var \Cake\Essentials\View\Helper\TextHelper
+     */
+    protected TextHelper $Text;
+
+    /**
+     * @inheritDoc
+     */
+    protected function setUp(): void
+    {
+        $this->Text = new TextHelper(new View());
+    }
+
+    #[Test]
+    #[TestWith(['This is a text' . PHP_EOL . 'This is another text'])]
+    #[TestWith([PHP_EOL . 'This is a text' . PHP_EOL . 'This is another text'])]
+    #[TestWith(['This is a text' . PHP_EOL . 'This is another text' . PHP_EOL])]
+    #[TestWith([PHP_EOL . 'This is a text' . PHP_EOL . 'This is another text' . PHP_EOL])]
+    #[TestWith([PHP_EOL . 'This is a text' . PHP_EOL . 'This is another text' . PHP_EOL . PHP_EOL])]
+    public function testAutoParagraph(string $text): void
+    {
+        $expected = '<p class="mb-2">This is a text</p>' . PHP_EOL .
+            '<p class="mb-0">This is another text</p>';
+        $result = $this->Text->autoParagraph($text);
+        $this->assertSame($expected, $result);
+    }
+
+    #[Test]
+    #[TestWith(['This is a text'])]
+    #[TestWith(['This is a text' . PHP_EOL])]
+    #[TestWith([PHP_EOL . 'This is a text'])]
+    #[TestWith([PHP_EOL . 'This is a text' . PHP_EOL])]
+    public function testAutoParagraphOnSingleLine(string $text): void
+    {
+        $expected = '<p class="mb-0">This is a text</p>';
+        $result = $this->Text->autoParagraph($text);
+        $this->assertSame($expected, $result);
+    }
+
+    #[Test]
+    #[TestWith([null])]
+    #[TestWith([''])]
+    public function testAutoParagraphOnEmptyValues(?string $text): void
+    {
+        $this->assertEmpty($this->Text->autoParagraph($text));
+    }
+
+    public static function provideHighlight(): Generator
+    {
+        yield [
+            'This is my text, with <span class="text-bg-warning">some</span> characters',
+            'some',
+        ];
+
+        yield [
+            'This is my text, with <div class="my-custom-class">some</div> characters',
+            'some',
+            ['format' => '<div class="my-custom-class">\1</div>'],
+        ];
+
+        yield [
+            '<span class="text-bg-warning">This</span> is my text, with some <span class="text-bg-warning">characters</span>',
+            ['characters', 'this'],
+        ];
+
+        yield [
+            '<div class="my-custom-class">This</div> is my text, with some <div class="my-custom-class">characters</div>',
+            ['characters', 'this'],
+            ['format' => '<div class="my-custom-class">\1</div>'],
+        ];
+    }
+
+    #[Test]
+    #[DataProvider('provideHighlight')]
+    public function testHighlight(string $expectedHighlight, string|array $phrase, array $options = []): void
+    {
+        $result = $this->Text->highlight(text: 'This is my text, with some characters', phrase: $phrase, options: $options);
+        $this->assertSame($expectedHighlight, $result);
+    }
+}
