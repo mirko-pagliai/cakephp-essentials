330ba27d2a63aae227532a34ff2b65e75ab7ef08
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-03-26 22:10:31 +0100

    added `GetSetTrait`

---
+++ src/ORM/Traits/GetSetTrait.php
@@ -0,0 +1,79 @@
+<?php
+declare(strict_types=1);
+
+namespace Cake\Essentials\ORM\Traits;
+
+use BadMethodCallException;
+use Cake\Datasource\Exception\MissingPropertyException;
+use Cake\Utility\Inflector;
+
+/**
+ * This trait implements `getOrFail()`, `isOrFail()` and all `getX()` and `isX()` magic method.
+ *
+ * These methods allow you to return the value of the entity's properties, throwing an exception when the property does
+ *  not exist or is `null`.
+ *
+ * @psalm-require-extends \Cake\ORM\Entity
+ */
+trait GetSetTrait
+{
+    /**
+     * Magic `__call()` method.
+     *
+     * Allows you to magically implement `getX()` and `isX()` methods for entities.
+     *
+     * For example, the `getCreated()` method will return the value of the `created` property.
+     * Instead, the `isActive()` method returns a boolean.
+     *
+     * These methods will explicitly call `getOrFail()`, then an exception will be thrown if the property does not
+     *  exist or is `null`.
+     * So, if you want an entity's `getX()` or `isX()` method to have a different behavior, you must explicitly declare
+     *  it in its class.
+     *
+     * @param string $name
+     * @param array $arguments
+     * @return mixed
+     * @throws \BadMethodCallException With a no existing method
+     * @throws \Cake\Datasource\Exception\MissingPropertyException When the property does not exist or is `null`
+     */
+    public function __call(string $name, array $arguments): mixed
+    {
+        if (str_starts_with($name, 'get')) {
+            return $this->getOrFail(property: Inflector::underscore(substr(string: $name, offset: 3)));
+        } elseif (str_starts_with($name, 'is')) {
+            return $this->isOrFail(property: Inflector::underscore(substr(string: $name, offset: 2)));
+        }
+
+        throw new BadMethodCallException(sprintf('Method `%s::%s()` does not exist. `get{PropertyName}()`/`is{PropertyName}()` expected.', $this::class, $name));
+    }
+
+    /**
+     * Returns the value of a field by name.
+     *
+     * Unlike the `get()` method, it throws an exception if the property does not exist or is `null`.
+     *
+     * @param string $property Property name
+     * @return mixed
+     * @throws \Cake\Datasource\Exception\MissingPropertyException When the property does not exist or is `null`
+     */
+    public function getOrFail(string $property): mixed
+    {
+        if (!isset($this->{$property})) {
+            throw new MissingPropertyException(['property' => $property, 'entity' => $this::class]);
+        }
+
+        return $this->{$property};
+    }
+
+    /**
+     * Returns the value of a field by name as boolean.
+     *
+     * @param string $property Property name
+     * @return bool
+     * @throws \Cake\Datasource\Exception\MissingPropertyException When the property does not exist or is `null`
+     */
+    public function isOrFail(string $property): bool
+    {
+        return (bool)$this->getOrFail($property);
+    }
+}
---
+++ tests/TestCase/ORM/Traits/GetSetTraitTest.php
@@ -0,0 +1,128 @@
+<?php
+declare(strict_types=1);
+
+namespace Cake\Essentials\Test\TestCase\ORM\Traits;
+
+use App\Model\Entity\EntityWithSomeVirtualFields;
+use BadMethodCallException;
+use Cake\Datasource\Exception\MissingPropertyException;
+use Cake\Essentials\ORM\Traits\GetSetTrait;
+use Cake\TestSuite\TestCase;
+use Override;
+use PHPUnit\Framework\Attributes\CoversTrait;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
+
+/**
+ * GetSetTraitTest.
+ */
+#[CoversTrait(GetSetTrait::class)]
+class GetSetTraitTest extends TestCase
+{
+    /**
+     * @var \App\Model\Entity\EntityWithSomeVirtualFields
+     */
+    protected EntityWithSomeVirtualFields $Entity;
+
+    /**
+     * @inheritDoc
+     */
+    #[Override]
+    protected function setUp(): void
+    {
+        $this->Entity = new EntityWithSomeVirtualFields();
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    #[Test]
+    #[TestWith(['getOrFail', 'getExampleProperty'])]
+    #[TestWith(['isOrFail', 'isExampleProperty'])]
+    public function testCallMagicMethod(string $expectedMethod, string $methodToCall): void
+    {
+        $Entity = $this->createPartialMock(EntityWithSomeVirtualFields::class, ['getOrFail', 'isOrFail']);
+        $Entity
+            ->expects($this->once())
+            ->method($expectedMethod)
+            ->with('example_property');
+
+        $Entity->{$methodToCall}();
+    }
+
+    #[Test]
+    public function testMagicCallGetMethodsWithNoExistingProperty(): void
+    {
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('Method `' . $this->Entity::class . '::noExistingMethod()` does not exist. `get{PropertyName}()`/`is{PropertyName}()` expected.');
+        $this->Entity->noExistingMethod();
+    }
+
+    #[Test]
+    #[TestWith(['a value'])]
+    #[TestWith([false])]
+    #[TestWith([''])]
+    #[TestWith([0])]
+    #[TestWith([0.0])]
+    #[TestWith([[]])]
+    public function testGetOrFail(mixed $value): void
+    {
+        $this->Entity->set('example_property', $value);
+        $this->assertSame($value, $this->Entity->getOrFail('example_property'));
+    }
+
+    #[Test]
+    #[TestWith(['null_property'])]
+    #[TestWith(['no_existing_property'])]
+    public function testGetOrFailWithNullableOrNoExistingProperty(string $nullableOrNoExistingProperty): void
+    {
+        $this->Entity->set('null_property');
+
+        $this->expectException(MissingPropertyException::class);
+        $this->expectExceptionMessage('Property `' . $nullableOrNoExistingProperty . '` does not exist for the entity `' . $this->Entity::class . '`');
+        $this->Entity->getOrFail($nullableOrNoExistingProperty);
+    }
+
+    #[Test]
+    public function testGetOrFailOnVirtualField(): void
+    {
+        $this->assertNotEmpty($this->Entity->getOrFail('stringable_virtual_field'));
+    }
+
+    #[Test]
+    public function testGetOrFailOnNullableVirtualField(): void
+    {
+        //First check that the virtual property exists and is `null`
+        $this->assertTrue($this->Entity->has('nullable_virtual_field'));
+        $this->assertNull($this->Entity->get('nullable_virtual_field'));
+
+        $this->expectException(MissingPropertyException::class);
+        $this->expectExceptionMessage('Property `nullable_virtual_field` does not exist for the entity `' . $this->Entity::class . '`');
+        $this->Entity->getOrFail('nullable_virtual_field');
+    }
+
+    /**
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
+    #[Test]
+    public function testIsOrFail(): void
+    {
+        $Entity = $this->createPartialMock(EntityWithSomeVirtualFields::class, ['getOrFail']);
+        $Entity
+            ->expects($this->once())
+            ->method('getOrFail')
+            ->with('example_property');
+
+        $Entity->isOrFail('example_property');
+    }
+
+    #[Test]
+    #[TestWith([true, true])]
+    #[TestWith([false, false])]
+    #[TestWith([true, 'a string'])]
+    public function testIsOrFailWithSomeValues(bool $expectedResult, mixed $value): void
+    {
+        $this->Entity->set('example_property', $value);
+        $this->assertSame($expectedResult, $this->Entity->isOrFail('example_property'));
+    }
+}
