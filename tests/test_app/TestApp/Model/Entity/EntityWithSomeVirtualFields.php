8ed3db9db9c7cb049f5779ffd74bf94b8df16403
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-03-26 22:29:57 +0100

    added file

---
+++ tests/test_app/TestApp/Model/Entity/EntityWithSomeVirtualFields.php
@@ -0,0 +1,25 @@
+?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\Essentials\ORM\Traits\GetSetTrait;
+use Cake\ORM\Entity;
+
+/**
+ * EntityWithSomeVirtualFields.
+ */
+class EntityWithSomeVirtualFields extends Entity
+{
+    use GetSetTrait;
+
+    protected function _getStringableVirtualField(): string
+    {
+        return 'a string';
+    }
+
+    protected function _getNullableVirtualField(): null
+    {
+        return null;
+    }
+}
