e80cdcfcf853bfdb192247d55977f587440609c6
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-04-03 20:54:08 +0200

    fixed

--- tests/TestCase/View/Helper/CollapsibleHelperTest.php
+++ tests/TestCase/View/Helper/CollapsibleHelperTest.php
@@ -61,7 +61,7 @@ class CollapsibleHelperTest extends TestCase
     #[Test]
     public function testLinkWithAlreadyOpen(): void
     {
-        $result = $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id', alreadyOpen: true);
+        $result = $this->Collapsible->link(title: 'Title', alreadyOpen: true);
         $this->assertStringContainsString('aria-expanded="true"', $result);
         $this->assertStringContainsString('>Title<i class="ms-1 bi bi-chevron-up"></i></a>', $result);
     }
@@ -69,15 +69,15 @@ class CollapsibleHelperTest extends TestCase
     #[Test]
     public function testLinkCustomClass(): void
     {
-        $result = $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id', options: ['class' => 'custom-class']);
+        $result = $this->Collapsible->link(title: 'Title', options: ['class' => 'custom-class']);
         $this->assertStringContainsString('class="custom-class text-decoration-none"', $result);
     }
 
     #[Test]
     public function testLinkWithIcon(): void
     {
-        $result = $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id', options: ['icon' => 'house']);
-        $this->assertStringContainsString('<i class="bi bi-house"></i> Title<i class="ms-1 bi bi-chevron-down"></i>', $result);
+        $result = $this->Collapsible->link(title: 'Title', options: ['icon' => 'house']);
+        $this->assertStringContainsString('><i class="bi bi-house"></i> Title<i class="ms-1 bi bi-chevron-down"></i></a>', $result);
     }
 
     #[Test]
@@ -86,7 +86,7 @@ class CollapsibleHelperTest extends TestCase
         $this->Collapsible->setConfig(key: 'toggleIcon.open', value: '1-circle');
         $this->Collapsible->setConfig(key: 'toggleIcon.close', value: '2-circle');
 
-        $result = $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id');
+        $result = $this->Collapsible->link(title: 'Title');
         $this->assertStringContainsString('bi bi-1-circle', $result);
         $this->assertStringContainsString('bi bi-2-circle', $result);
         $this->assertStringContainsString('onclick=', $result);
@@ -105,7 +105,7 @@ class CollapsibleHelperTest extends TestCase
          * `onclick` attribute is not present.
          * There is no `text-decoration-none` class.
          */
-        $result = $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id');
+        $result = $this->Collapsible->link(title: 'Title');
         $this->assertStringContainsString('>Title</a>', $result);
         $this->assertStringNotContainsString('onclick=', $result);
         $this->assertStringNotContainsString('text-decoration-none', $result);
@@ -115,7 +115,7 @@ class CollapsibleHelperTest extends TestCase
          * `onclick` attribute is not present.
          * There is the `text-decoration-none` class (because of the normal icon).
          */
-        $result = $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id', options: ['icon' => 'house']);
+        $result = $this->Collapsible->link(title: 'Title', options: ['icon' => 'house']);
         $this->assertStringContainsString('><i class="bi bi-house"></i> Title</a>', $result);
         $this->assertStringNotContainsString('onclick=', $result);
         $this->assertStringContainsString('class="text-decoration-none"', $result);
