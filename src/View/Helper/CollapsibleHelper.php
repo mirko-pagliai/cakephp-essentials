294696895842fcda72f97ee47591d6e0cf20214c
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-04-03 13:05:03 +0200

    updated

--- src/View/Helper/CollapsibleHelper.php
+++ src/View/Helper/CollapsibleHelper.php
@@ -3,12 +3,92 @@ declare(strict_types=1);
 
 namespace Cake\Essentials\View\Helper;
 
+use BadMethodCallException;
 use Cake\View\Helper;
 
 /**
  * CollapsibleHelper.
+ *
+ * @property \Cake\Essentials\View\Helper\HtmlHelper $Html
  */
 class CollapsibleHelper extends Helper
 {
+    public bool $alreadyOpen;
 
+    public string $collapsibleId;
+
+    /**
+     * @var array<string, mixed>
+     */
+    protected array $_defaultConfig = [
+        /**
+         * Icons that will be appended to the link and will allow you to open/close the collapsible
+         *
+         * Not to be confused with the link icon set using the `$options` argument.
+         */
+        'icon' => [
+            'open' => ['name' => 'chevron-up', 'class' => 'ms-1'],
+            'close' => ['name' => 'chevron-down', 'class' => 'ms-1'],
+        ],
+    ];
+
+    /**
+     * @var array<string>
+     */
+    protected array $helpers = ['Html'];
+
+    public function link(string $title, string $collapsibleId = '', array $options = [], bool $alreadyOpen = false): string
+    {
+        $this->alreadyOpen = $alreadyOpen;
+        $this->collapsibleId = $collapsibleId ?: uniqid('collapsible-');
+
+        /**
+         * Builds open and close icons.
+         *
+         * These will then be handled via the `onclick` options that will use javascript to replace the icon.
+         */
+        $openIcon = $this->Html->buildIcon(['icon' => $this->getConfigOrFail('icon.open')]);
+        $closeIcon = $this->Html->buildIcon(['icon' => $this->getConfigOrFail('icon.close')]);
+
+        $options += [
+            'role' => 'button',
+        ];
+
+        $options = $this->addClass(options: $options, class: 'text-decoration-none');
+
+        return $this->Html->link(
+            title: $title . ($this->alreadyOpen ? $openIcon : $closeIcon),
+            url: '#' . $this->collapsibleId,
+            options: [
+                'aria-controls' => $this->collapsibleId,
+                'aria-expanded' => $this->alreadyOpen ? 'true' : 'false',
+                'data-bs-toggle' => 'collapse',
+                'onclick' => 'javascript:$(\'i\', this).replaceWith($(this).hasClass(\'collapsed\') ? \'' . htmlentities($closeIcon) . '\' : \'' . htmlentities($openIcon) . '\')',
+            ] + $options
+        );
+    }
+
+    /**
+     * Creates the contents of the collapse block.
+     *
+     * You must first have correctly called the `link()` method, which sets the necessary properties.
+     *
+     * @param array<string>|string $content
+     * @param array $options
+     * @return string
+     */
+    public function content(string|array $content, array $options = []): string
+    {
+        if (!isset($this->alreadyOpen) || !isset($this->collapsibleId)) {
+            throw new BadMethodCallException('Seems that the link to open the collapsible was not set, perhaps the `link()` method was not called?');
+        }
+
+        if (is_array($content)) {
+            $content = implode('<br />', $content);
+        }
+
+        $options = $this->addClass(options: $options, class: $this->alreadyOpen ? 'collapse show' : 'collapse');
+
+        return $this->Html->div(text: $content, options: ['id' => $this->collapsibleId] + $options);
+    }
 }
--- tests/TestCase/View/Helper/CollapsibleHelperTest.php
+++ tests/TestCase/View/Helper/CollapsibleHelperTest.php
@@ -3,11 +3,15 @@ declare(strict_types=1);
 
 namespace Cake\Essentials\Test\TestCase\View\Helper;
 
+use BadMethodCallException;
 use Cake\Essentials\View\Helper\CollapsibleHelper;
+use Cake\Essentials\View\Helper\HtmlHelper;
+use Cake\Essentials\View\View;
 use Cake\TestSuite\TestCase;
-use Cake\View\View;
 use Override;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\Attributes\TestWith;
 
 /**
  * CollapsibleHelperTest.
@@ -21,11 +25,66 @@ class CollapsibleHelperTest extends TestCase
     protected CollapsibleHelper $Collapsible;
 
     /**
+     * @var \Cake\Essentials\View\Helper\HtmlHelper&\PHPUnit\Framework\MockObject\MockObject
+     */
+    protected HtmlHelper $HtmlHelper;
+
+    /**
      * @inheritDoc
      */
     #[Override]
     protected function setUp(): void
     {
-        $this->Collapsible = new CollapsibleHelper(new View());
+        $View = new View();
+
+        $this->HtmlHelper = $this->getMockBuilder(HtmlHelper::class)
+            ->setConstructorArgs([$View])
+            ->onlyMethods(['link'])
+            ->getMock();
+
+        $this->Collapsible = new CollapsibleHelper($View);
+        $this->Collapsible->getView()->set('Html', $this->HtmlHelper);
+    }
+
+    public function testLink(): void
+    {
+        $this->HtmlHelper
+            ->expects($this->once())
+            ->method('link')
+            ->with('Title');
+
+        $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id', options: [], alreadyOpen: false);
+    }
+
+    #[Test]
+    #[TestWith(['<div id="my-id" class="collapse">Text</div>', 'Text'])]
+    #[TestWith(['<div id="my-id" class="collapse">First<br />Second</div>', ['First', 'Second']])]
+    #[TestWith(['<div id="my-id" class="custom-class collapse">Text</div>', 'Text', ['class' => 'custom-class']])]
+    public function testContent(string $expectedContent, string|array $content, array $options = []): void
+    {
+        $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id');
+
+        $result = $this->Collapsible->content($content, $options);
+        $this->assertSame($expectedContent, $result);
+    }
+
+    #[Test]
+    #[TestWith(['<div id="my-id" class="collapse show">Text</div>', 'Text'])]
+    #[TestWith(['<div id="my-id" class="collapse show">First<br />Second</div>', ['First', 'Second']])]
+    #[TestWith(['<div id="my-id" class="custom-class collapse show">Text</div>', 'Text', ['class' => 'custom-class']])]
+    public function testContentAlreadyOpened(string $expectedContent, string|array $content, array $options = []): void
+    {
+        $this->Collapsible->link(title: 'Title', collapsibleId: 'my-id', alreadyOpen: true);
+
+        $result = $this->Collapsible->content($content, $options);
+        $this->assertSame($expectedContent, $result);
+    }
+
+    #[Test]
+    public function testContentWithoutLinkBeingCalled(): void
+    {
+        $this->expectException(BadMethodCallException::class);
+        $this->expectExceptionMessage('Seems that the link to open the collapsible was not set, perhaps the `link()` method was not called?');
+        $this->Collapsible->content('Text');
     }
 }
