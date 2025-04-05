5e64c0e0be5158f759f3499dcc706f41f33bcfcc
mirko-pagliai <mirko.pagliai@gmail.com>
2025-04-05 11:14:58 +0200

    added `link()`, `linkFromPath()` and `postLink()` methods

--- src/View/Helper/DropdownHelper.php
+++ src/View/Helper/DropdownHelper.php
@@ -8,12 +8,72 @@ use Cake\View\Helper;
 /**
  * DropdownHelper.
  *
+ * @property \Cake\Essentials\View\Helper\FormHelper $Form
  * @property \Cake\Essentials\View\Helper\HtmlHelper $Html
  */
 class DropdownHelper extends Helper
 {
+    protected array $_links = [];
+
+    protected string $_opening = '';
+
     /**
      * @var array
      */
-    protected array $helpers = ['Html'];
+    protected array $helpers = ['Html', 'Form'];
+
+    /**
+     * Creates an HTML link, compatible with the dropdown submenu.
+     *
+     * @param string $title
+     * @param array|string $url
+     * @param array<string, mixed> $options
+     * @return self
+     *
+     * @see \App\View\Helper\HtmlHelper::link() for more information about the method arguments
+     */
+    public function link(string $title, array|string $url, array $options = []): self
+    {
+        $options = $this->addClass(options: $options, class: 'dropdown-item');
+
+        $this->_links[] = $this->Html->link(title: $title, url: $url, options: $options);
+
+        return $this;
+    }
+
+    /**
+     * Creates an HTML link from route path string, compatible with the dropdown submenu.
+     *
+     * @param string $title
+     * @param string $path
+     * @param array $params
+     * @param array<string, mixed> $options
+     * @return self
+     *
+     * @see \App\View\Helper\HtmlHelper::linkPath() for more information about the method arguments
+     */
+    public function linkFromPath(string $title, string $path, array $params = [], array $options = []): self
+    {
+        return $this->link(title: $title, url: ['_path' => $path] + $params, options: $options);
+    }
+
+    /**
+     * Creates an HTML link, but access the URL using the method you specify (defaults to POST), compatible with
+     *  the dropdown submenu.
+     *
+     * @param string $title
+     * @param array|string $url
+     * @param array<string, mixed> $options
+     * @return self
+     *
+     * @see \Cake\View\Helper\FormHelper::postLink() for more information about the method arguments
+     */
+    public function postLink(string $title, array|string $url, array $options = []): self
+    {
+        $options = $this->addClass(options: $options, class: 'dropdown-item');
+
+        $this->_links[] = $this->Form->postLink(title: $title, url: $url, options: $options);
+
+        return $this;
+    }
 }
--- tests/TestCase/View/Helper/DropdownHelperTest.php
+++ tests/TestCase/View/Helper/DropdownHelperTest.php
@@ -4,11 +4,13 @@ declare(strict_types=1);
 namespace Cake\Essentials\Test\TestCase\View\Helper;
 
 use Cake\Essentials\View\Helper\DropdownHelper;
+use Cake\Essentials\View\Helper\FormHelper;
 use Cake\Essentials\View\Helper\HtmlHelper;
 use Cake\Essentials\View\View;
 use Cake\TestSuite\TestCase;
 use Override;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\UsesClass;
 
 /**
@@ -30,5 +32,72 @@ class DropdownHelperTest extends TestCase
     protected function setUp(): void
     {
         $this->Dropdown = new DropdownHelper(new View());
+    }
+
+    #[Test]
+    public function testLink(): void
+    {
+        $HtmlHelper = $this->createPartialMock(HtmlHelper::class, ['link']);
+        $HtmlHelper
+            ->expects($this->once())
+            ->method('link')
+            ->with(
+                'My link',
+                '#',
+                ['class' => 'my-custom-class dropdown-item']
+            );
+
+        $this->Dropdown->getView()->helpers()->set('Html', $HtmlHelper);
+
+        $this->Dropdown->link(
+            title: 'My link',
+            url: '#',
+            options: ['class' => 'my-custom-class']
+        );
+    }
+
+    #[Test]
+    public function testLinkFromPath(): void
+    {
+        $HtmlHelper = $this->createPartialMock(HtmlHelper::class, ['link']);
+        $HtmlHelper
+            ->expects($this->once())
+            ->method('link')
+            ->with(
+                'My link from path',
+                ['_path' => 'Users::index', '?' => ['k' => 'v']],
+                ['class' => 'my-custom-class dropdown-item']
+            );
+
+        $this->Dropdown->getView()->helpers()->set('Html', $HtmlHelper);
+
+        $this->Dropdown->linkFromPath(
+            title: 'My link from path',
+            path: 'Users::index',
+            params: ['?' => ['k' => 'v']],
+            options: ['class' => 'my-custom-class']
+        );
+    }
+
+    #[Test]
+    public function testPostLink(): void
+    {
+        $FormHelper = $this->createPartialMock(FormHelper::class, ['postLink']);
+        $FormHelper
+            ->expects($this->once())
+            ->method('postLink')
+            ->with(
+                'My post link',
+                '#',
+                ['class' => 'my-custom-class dropdown-item']
+            );
+
+        $this->Dropdown->getView()->helpers()->set('Form', $FormHelper);
+
+        $this->Dropdown->postLink(
+            title: 'My post link',
+            url: '#',
+            options: ['class' => 'my-custom-class']
+        );
     }
 }
