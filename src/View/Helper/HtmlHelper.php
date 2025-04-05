5bf317a1f2dce8b9fe1bd94107c133b4c2d46fdd
mirko-pagliai <mirko.pagliai@gmail.com>
2025-04-05 17:40:33 +0200

    added icon constants

--- src/View/Helper/HtmlHelper.php
+++ src/View/Helper/HtmlHelper.php
@@ -31,6 +31,10 @@ use Override;
  */
 class HtmlHelper extends BootstrapUIHtmlHelper
 {
+    public const string ERROR_ICON = 'x-circle';
+
+    public const string ERROR_ICON_FILL = 'x-circle-fill';
+
     public const string SUCCESS_ICON = 'check-circle';
 
     public const string SUCCESS_ICON_FILL = 'check-circle-fill';
