6d49cc1a8aa4da18483ed94a5921b0f293032a17
mirko-pagliai <mirko.pagliai@gmail.com>
2025-04-09 10:56:10 +0200

    added UsersGroupMethodsTrait

--- tests/test_app/TestApp/Model/Entity/User.php
+++ tests/test_app/TestApp/Model/Entity/User.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace App\Model\Entity;
 
 use Cake\Essentials\ORM\Entity\Traits\GetSetTrait;
+use Cake\Essentials\ORM\Entity\Traits\UsersGroupMethodsTrait;
 use Cake\Essentials\ORM\Entity\Traits\UserStatusMethodsTrait;
 use Cake\ORM\Entity;
 
@@ -11,11 +12,14 @@ use Cake\ORM\Entity;
  * User Entity.
  *
  * @property \Cake\Essentials\ORM\Enum\UserStatus $status
+ * @property \App\Model\Entity\UsersGroup $users_group
  *
  * @method \Cake\Essentials\ORM\Enum\UserStatus getStatus()
+ * @method \App\Model\Entity\UsersGroup getUsersGroup()
  */
 class User extends Entity
 {
     use GetSetTrait;
+    use UsersGroupMethodsTrait;
     use UserStatusMethodsTrait;
 }
