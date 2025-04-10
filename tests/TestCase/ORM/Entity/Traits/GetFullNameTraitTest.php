4b022c841b3c4886a6cdd5e511520694eb86cd38
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-04-10 12:43:00 +0200

    fixed

--- tests/TestCase/ORM/Entity/Traits/GetFullNameTraitTest.php
+++ tests/TestCase/ORM/Entity/Traits/GetFullNameTraitTest.php
@@ -3,9 +3,11 @@ declare(strict_types=1);
 
 namespace Cake\Essentials\Test\TestCase\ORM\Entity\Traits;
 
-use App\Model\Entity\Person;
+use Cake\Essentials\ORM\Entity\EntityWithGetSetInterface;
 use Cake\Essentials\ORM\Entity\Traits\GetFullNameTrait;
+use Cake\Essentials\ORM\Entity\Traits\GetSetTrait;
 use Cake\Essentials\TestSuite\TestCase;
+use Cake\ORM\Entity;
 use PHPUnit\Framework\Attributes\CoversTrait;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\Attributes\TestWith;
@@ -16,6 +18,19 @@ use PHPUnit\Framework\Attributes\TestWith;
 #[CoversTrait(GetFullNameTrait::class)]
 class GetFullNameTraitTest extends TestCase
 {
+    /**
+     * @var \Cake\Essentials\ORM\Entity\EntityWithGetSetInterface
+     */
+    protected EntityWithGetSetInterface $Person;
+
+    protected function setUp(): void
+    {
+        $this->Person = new class extends Entity implements EntityWithGetSetInterface {
+            use GetFullNameTrait;
+            use GetSetTrait;
+        };
+    }
+
     #[Test]
     #[TestWith(['Red Mark', 'Red', 'Mark'])]
     #[TestWith([null, 'Red', null])]
@@ -23,16 +38,15 @@ class GetFullNameTraitTest extends TestCase
     #[TestWith([null, null, null])]
     public function testGetFullNameVirtualField(?string $expectedFullName, ?string $lastName, ?string $firstName): void
     {
-        $Person = new Person(['last_name' => $lastName, 'first_name' => $firstName]);
-
-        $this->assertSame($expectedFullName, $Person->full_name);
+        $this->Person->set(['last_name' => $lastName, 'first_name' => $firstName]);
+        $this->assertSame($expectedFullName, $this->Person->get('full_name'));
     }
 
     #[Test]
     public function testGetFullName(): void
     {
-        $Person = new Person(['last_name' => 'Red', 'first_name' => 'Mark']);
-
-        $this->assertSame('Red Mark', $Person->getFullName());
+        $this->Person->set(['last_name' => 'Red', 'first_name' => 'Mark']);
+        /** @phpstan-ignore method.notFound */
+        $this->assertSame('Red Mark', $this->Person->getFullName());
     }
 }
