483d69d0e6771fa8d1befafd05cdcacf08b86cd9
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-03 09:31:16 +0200

    added `AssertPolicyTrait` to provide assertion capabilities for testing policy methods

---
+++ src/TestSuite/Traits/AssertPolicyTrait.php
@@ -0,0 +1,67 @@
+<?php
+declare(strict_types=1);
+
+namespace Cake\Essentials\TestSuite\Traits;
+
+use Authentication\IdentityInterface;
+use Cake\Datasource\EntityInterface;
+use Cake\ORM\Entity;
+
+/**
+ * Trait providing assertion capabilities to validate the results of policy method executions.
+ *
+ * This trait is typically used in testing scenarios to ensure that policy methods (such as those
+ * used for authorization checks) return the expected results when provided with specific input.
+ */
+trait AssertPolicyTrait
+{
+    /**
+     * Asserts the result of a policy method execution against the expected result.
+     *
+     * Example:
+     * ```
+     * $this->assertPolicyResult(true, ArticlePolicy::class, 'canAdd', 'admin');
+     * ```
+     * This asserts that the method `ArticlePolicy::canAdd()`, for the user group `admin`, returns `true`.
+     *
+     * @param bool $expectedResult The expected boolean result of the policy method.
+     * @param object|string $Policy The policy object or the class name of the policy to execute.
+     * @param string $method The method name to call on the policy object.
+     * @param \Authentication\IdentityInterface&\Cake\Datasource\EntityInterface $Identity The identity object used when invoking the policy method.
+     * @param \Cake\Datasource\EntityInterface|null $Entity Optional entity object passed to the policy method. Defaults to null.
+     *
+     * @return void The method does not return a value but performs an assertion.
+     */
+    public function assertPolicyResult(
+        bool $expectedResult,
+        object|string $Policy,
+        string $method,
+        IdentityInterface&EntityInterface $Identity,
+        ?EntityInterface $Entity = null,
+    ): void {
+        if (!is_object($Policy)) {
+            $Policy = new $Policy();
+        }
+
+        if (!method_exists($Policy, $method)) {
+            $this->fail(sprintf('Policy `%s` does not have a `%s()` method', $Policy::class, $method));
+        }
+
+        //Runs the policy and gets `$result`
+        $result = $Policy->{$method}($Identity, $Entity ?: new Entity());
+
+        $message = sprintf(
+            '`%s::%s()` method has returned `%s`',
+            $Policy::class,
+            $method,
+            $result ? 'true' : 'false'
+        );
+        /** @var \Cake\Datasource\EntityInterface|null $UsersGroup */
+        $UsersGroup = $Identity->get('users_group');
+        if ($UsersGroup && is_string($UsersGroup->get('name'))) {
+            $message .= ' for group `' . $UsersGroup->get('name') . '`';
+        }
+
+        $this->assertSame($expectedResult, $result, $message);
+    }
+}
