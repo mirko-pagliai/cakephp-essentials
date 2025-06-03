05e4a6792e9a6060f4a5e512f636d4188dad41f2
Mirko Pagliai <mirko.pagliai@gmail.com>
2025-06-03 09:33:12 +0200

    added `UserIdentityTrait` and implemented `UserIdentityInterface` in `User` entity

--- tests/test_app/TestApp/Model/Entity/User.php
+++ tests/test_app/TestApp/Model/Entity/User.php
@@ -3,9 +3,12 @@ declare(strict_types=1);
 
 namespace App\Model\Entity;
 
+use Authorization\IdentityInterface;
 use Cake\Essentials\ORM\Entity\Traits\GetSetTrait;
+use Cake\Essentials\ORM\Entity\Traits\UserIdentityTrait;
 use Cake\Essentials\ORM\Entity\Traits\UserMethodsTrait;
 use Cake\Essentials\ORM\Entity\Traits\UserStatusMethodsTrait;
+use Cake\Essentials\ORM\Entity\UserIdentityInterface;
 use Cake\Essentials\ORM\Entity\UserInterface;
 use Cake\ORM\Entity;
 
@@ -17,9 +20,10 @@ use Cake\ORM\Entity;
  *
  * @method \Cake\Essentials\ORM\Enum\UserStatus getStatus()
  */
-class User extends Entity implements UserInterface
+class User extends Entity implements UserIdentityInterface, UserInterface
 {
     use GetSetTrait;
+    use UserIdentityTrait;
     use UserMethodsTrait;
     use UserStatusMethodsTrait;
 }
