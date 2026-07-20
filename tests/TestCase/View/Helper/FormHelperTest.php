d7543d1d8a7b047b3376cff4fe5976d034ff9722
mirko-pagliai <mirko.pagliai@gmail.com>
2026-07-20 14:42:31 +0200

    updated dependencies in composer.lock: bumped versions for CakePHP, composer/ca-bundle, and added psr/link package

--- tests/TestCase/View/Helper/FormHelperTest.php
+++ tests/TestCase/View/Helper/FormHelperTest.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 namespace Cake\Essentials\Test\TestCase\View\Helper;
 
+use Cake\Core\Configure;
 use Cake\Essentials\View\Helper\FormHelper;
 use Cake\Essentials\View\Helper\HtmlHelper;
 use Cake\TestSuite\TestCase;
@@ -201,16 +202,24 @@ class FormHelperTest extends TestCase
     public function testPostLink(): void
     {
         $expected = [
-            'form' => ['name', 'method' => 'post', 'style' => 'display:none;', 'action' => '#url'],
+            'form' => ['name', 'method' => 'post', 'hidden' => 'hidden', 'action' => '#url'],
             'input' => ['type' => 'hidden', 'name' => '_method', 'value' => 'POST'],
             '/form',
-            'a' => ['href' => '#', 'onclick', 'class' => 'text-decoration-none', 'title' => 'Title'],
+            'a' => ['href' => '#', 'class' => 'text-decoration-none', 'onclick', 'title' => 'Title'],
             'i' => ['class' => 'bi bi-home'],
             '/i',
             'Title',
             '/a',
         ];
+
+        if (version_compare(Configure::version(), '5.4', '<')) {
+            $expected['form'] = ['name', 'method' => 'post', 'style' => 'display:none;', 'action' => '#url'];
+            $expected['a'] = ['href' => '#', 'onclick', 'class' => 'text-decoration-none', 'title' => 'Title'];
+        }
+//        debug($expected);
+
         $result = $this->Form->postLink(title: 'Title', url: '#url', options: ['icon' => 'home']);
+//        dd($result);
         $this->assertHtml($expected, $result);
     }
 
